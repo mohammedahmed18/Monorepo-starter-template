2c24a649d1cdbfa9902e6378ab8ba07a54372505
mohammedahmed18 <mohammed18200118@gmail.com>
2023-08-25 01:26:55 +0300

    run the next server instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-# copy the frontend build to the nginx folder
-cp -r dist/apps/docit/exported/* /usr/share/nginx/html
+# remove the default nginx index.html
+rm -rf /usr/share/nginx/html/*
 
 # Start Nginx server
 nginx -g "daemon off;" &
+
+
+# cd into the dist folder where the compiled react code is
+cd dist/apps/docit
+npm set-script start "next start -p 4200"
+
+# start the client server in the background
+npm run start &
 
 # Start backend server
 node --enable-source-maps dist/apps/server/main.js
