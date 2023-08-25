1c2f2afd46a7e58fa77e9d806b6e887c091470ce
mohammedahmed18 <mohammed18200118@gmail.com>
2023-08-25 04:09:06 +0300

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,81 +1,76 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-# declare the environment variables from the .env file
-if [ -f "./.env" ]
-then
-  declare $(grep -v '^#' ./.env | xargs) 
+# Load environment variables from the .env file
+if [ -f "./.env" ]; then
+  export $(grep -v '^#' ./.env | xargs)
 fi
 
-
-
-# show the booting page until the server is ready
-booting_page="templates/booting.html"
-cp "$booting_page" /var/www/html/index.nginx-debian.html
-nginx
-
-
-NGINX_CONF_FILE=/etc/nginx/nginx.conf
-
+# Set default values if environment variables are not defined
 SERVER_PORT=${SERVER_PORT:-3000}
 CLIENT_PORT=${CLIENT_PORT:-4200}
 
-
-# add the nginx configuration to the nginx configuration file
-echo "
-    events { worker_connections 1024; }
-
-   http {
-    include /etc/nginx/mime.types;
-    client_max_body_size 4m;
-
-    server {
-        server_name localhost;
-        root /usr/share/nginx/html;
-        index index.html;
-
-        location /api/ {
-          proxy_pass http://localhost:$SERVER_PORT/;
-        }
-
-        location / {
-            proxy_pass http://localhost:$CLIENT_PORT/;
-        }
-
-        listen 80 default_server;
-        listen [::]:80;
-    }
-    }
-
-" > "$NGINX_CONF_FILE"
-
-# remove the default nginx index.html
-rm -rf /usr/share/nginx/html/*
-
-# save the current working directory as the root directory
+# Define paths
 ROOT_DIR=$(pwd)
 SERVER_DIR="$ROOT_DIR/dist/apps/server"
 CLIENT_DIR="$ROOT_DIR/dist/apps/docit"
-
-wait_for_it_dir="$ROOT_DIR/scripts/wait-for-it.sh"
-
-# make the wait-for-it.sh script executable
-chmod +x "$wait_for_it_dir"
+BOOTING_PAGE="templates/booting.html"
+NGINX_CONF_FILE="/etc/nginx/nginx.conf"
+WAIT_FOR_IT_SCRIPT="$ROOT_DIR/scripts/wait-for-it.sh"
 
 
-# Wait for a certain duration before proceeding
-sleep 7
+# Make the wait-for-it.sh script executable
+chmod +x "$WAIT_FOR_IT_SCRIPT"
 
+# Copy the booting page to NGINX default location
+cp "$BOOTING_PAGE" /var/www/html/index.nginx-debian.html
+nginx
+
+# NGINX configuration
+NGINX_CONFIG="
+events { worker_connections 1024; }
+
+http {
+  include /etc/nginx/mime.types;
+  client_max_body_size 4m;
+
+  server {
+    server_name localhost;
+    root /usr/share/nginx/html;
+    index index.html;
+
+    location /api/ {
+      proxy_pass http://localhost:$SERVER_PORT/;
+    }
+
+    location / {
+      proxy_pass http://localhost:$CLIENT_PORT/;
+    }
+
+    listen 80 default_server;
+    listen [::]:80;
+  }
+}
+"
+
+# Apply NGINX configuration
+echo "$NGINX_CONFIG" > "$NGINX_CONF_FILE"
+
+# Remove the default NGINX index.html
+rm -rf /usr/share/nginx/html/*
 
 # Start backend server
-cd $SERVER_DIR && node --enable-source-maps main.js &
+cd "$SERVER_DIR" && node --enable-source-maps main.js &
 
-# wait for the server to start then start the client
-wait_for_it_dir localhost:$SERVER_PORT -t 0 -- cd $CLIENT_DIR \
- && npm set-script start "next start -p $CLIENT_PORT" \ 
- && npm run start &
+# Wait for backend server to start
+$WAIT_FOR_IT_SCRIPT localhost:$SERVER_PORT -t 0
 
+# Start frontend client
+cd "$CLIENT_DIR" && npm set-script start "next start -p $CLIENT_PORT" && npm run start &
 
-# wait for the client to start then start the nginx server (at this point both the client and server are running)
-wait_for_it_dir localhost:$CLIENT_PORT -t 0 -- nginx -g "daemon off;"
+# Wait for frontend client to start
+$WAIT_FOR_IT_SCRIPT localhost:$CLIENT_PORT -t 0
+
+# Start NGINX (both frontend and backend are running at this point)
+nginx -g "daemon off;"
