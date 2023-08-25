5b31263f06f51a2912388fdc5b5d386921884b34
mohammedahmed18 <mohammed18200118@gmail.com>
2023-08-25 03:30:03 +0300

    chore: nginx configuration and booting page

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,72 @@
 #!/bin/sh
 
+# declare the environment variables from the .env file
+if [ -f "./.env" ]
+then
+  declare $(grep -v '^#' ./.env | xargs) 
+fi
+
+
+
+init_loading_pages(){
+  local booting_page="templates/booting.html"
+  cp "$booting_page" /var/www/html/index.nginx-debian.html
+  # Start nginx page to display the Appsmith is Initializing page
+  nginx
+}
+
+init_loading_pages
+
+NGINX_CONF_FILE=/etc/nginx/nginx.conf
+
+SERVER_PORT=${SERVER_PORT:-3000}
+CLIENT_PORT=${CLIENT_PORT:-4200}
+
+
+# add the nginx configuration to the nginx configuration file
+echo "
+    events { worker_connections 1024; }
+
+   http {
+    include /etc/nginx/mime.types;
+    client_max_body_size 4m;
+
+    server {
+        server_name localhost;
+        root /usr/share/nginx/html;
+        index index.html;
+
+        location /api/ {
+          proxy_pass http://localhost:$SERVER_PORT/;
+        }
+
+        location / {
+            proxy_pass http://localhost:$CLIENT_PORT/;
+        }
+
+        listen 80 default_server;
+        listen [::]:80;
+    }
+    }
+
+" > "$NGINX_CONF_FILE"
+
 # remove the default nginx index.html
 rm -rf /usr/share/nginx/html/*
 
-# Start Nginx server
-nginx -g "daemon off;" &
 
+cd dist/apps/server
 
-# cd into the dist folder where the compiled react code is
-cd dist/apps/docit
-npm set-script start "next start -p 4200"
-
-# start the client server in the background
-npm run start &
-
-# cd into the server
-cd ../server
+sleep 7
 
 # Start backend server
-node --enable-source-maps main.js
+node --enable-source-maps main.js &
+
+
+./scripts/wait-for-it.sh localhost:$SERVER_PORT -t 0 -- cd ../docit \
+ && npm set-script start "next start -p $CLIENT_PORT" \ 
+ && npm run start &
+
+
+# wait for the client to start then start nginx
+./scripts/wait-for-it.sh localhost:$CLIENT_PORT -t 0 -- nginx -g "daemon off;"
