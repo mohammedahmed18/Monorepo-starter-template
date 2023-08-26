3aae2738f8ea9cda743a389931a57314c065c37a
mohammedahmed18 <mohammed18200118@gmail.com>
2023-08-26 17:36:20 +0300

    don't issue a new certificate if there is already one

--- deploy/letsencrypt/certificate-request.sh
+++ deploy/letsencrypt/certificate-request.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 CUSTOM_DOMAIN="$1"
-# Request from Lets Encrypt
-certbot certonly --webroot --webroot-path="/var/www/html" \
-    --register-unsafely-without-email \
-    --domains $CUSTOM_DOMAIN \
-    --rsa-key-size 4096 \
-    --agree-tos \
-    --force-renewal
 
-if (($? != 0)); then
-    echo "ERROR: certbot request failed for $CUSTOM_DOMAIN use http on port 80 - exiting"
-    exit 1
+if [[ -f "/etc/letsencrypt/live/$CUSTOM_DOMAIN/fullchain.pem" ]]; then
+    echo "Certificate already exists for $CUSTOM_DOMAIN - exiting"
 else
-    cp /usr/app/deploy/letsencrypt/options-ssl-nginx.conf /etc/letsencrypt/options-ssl-nginx.conf
-    cp /usr/app/deploy/letsencrypt/ssl-dhparams.pem /etc/letsencrypt/ssl-dhparams.pem
-    cp /usr/app/deploy/letsencrypt/nginx-ssl.conf /etc/nginx/sites-available/nginx-ssl.conf
-    sed -i "s/CUSTOM_DOMAIN/$CUSTOM_DOMAIN/g" /etc/nginx/sites-available/nginx-ssl.conf
-    ln -s /etc/nginx/sites-available/nginx-ssl.conf /etc/nginx/sites-enabled/nginx-ssl.conf
+
+    # Request from Lets Encrypt
+    certbot certonly --webroot --webroot-path="/var/www/html" \
+        --register-unsafely-without-email \
+        --domains $CUSTOM_DOMAIN \
+        --rsa-key-size 4096 \
+        --agree-tos \
+        --force-renewal
+
+    if (($? != 0)); then
+        echo "ERROR: certbot request failed for $CUSTOM_DOMAIN use http on port 80 - exiting"
+        exit 1
+    else
+        cp /usr/app/deploy/letsencrypt/options-ssl-nginx.conf /etc/letsencrypt/options-ssl-nginx.conf
+        cp /usr/app/deploy/letsencrypt/ssl-dhparams.pem /etc/letsencrypt/ssl-dhparams.pem
+        cp /usr/app/deploy/letsencrypt/nginx-ssl.conf /etc/nginx/sites-available/nginx-ssl.conf
+        sed -i "s/CUSTOM_DOMAIN/$CUSTOM_DOMAIN/g" /etc/nginx/sites-available/nginx-ssl.conf
+        ln -s /etc/nginx/sites-available/nginx-ssl.conf /etc/nginx/sites-enabled/nginx-ssl.conf
+    fi
+
 fi
